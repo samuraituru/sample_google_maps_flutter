8763531e20d162591fa2d7007254eae63c3f90df
“samuraituru” <samuraituru.dev@gmail.com>
2022-08-04 08:50:02 +0900

    MAP-API-KEYに対応

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,11 +4,12 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  let mapAPIKey = Bundle.main.object(forInfoDictionaryKey: "MapAPIKey") as! String
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("My-API-Key")
+    GMSServices.provideAPIKey(mapAPIKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
